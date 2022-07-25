ae109508644a8d61cc311541375740a230b3077d
Maxime Sourdin <maxime@sourdin.ovh>
2022-07-25 12:26:16 +0000

    Update main.py, Dockerfile, .gitlab-ci.yml

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
-from mastodon import Mastodon
 from mastodon import Mastodon, StreamListener
 from bs4 import BeautifulSoup
-import re,os, markovify, json, time, schedule, logging, threading, requests
-
-class Stream(StreamListener):
-    def __init__(self): #Héritage
-        super(Stream, self).__init__()
-        # self.logger = logging.getLogger
-
-    def on_notification(self,notif): #Appelé lorsqu'une notification arrive
-        if notif['type'] == 'mention': #Vérifiez si le contenu de la notification est une réponse
-            content = notif['status']['content'] #C'est le corps principal de la réponse
-            id = notif['status']['account']['username']
-            st = notif['status']
-            main(content, st, id)
+import re,os, markovify, json, threading, random, time, datetime
 
 #if not path.exists("clientcred.secret"):
 #        print("No clientcred.secret, registering application")
@@ -76,6 +63,7 @@ def get_toots(client, id):
     toots = client.account_statuses(id)
     while toots is not None and len(toots) > 0:
         for toot in toots:
+            print(toot)
             t = parse_toot(toot)
             if t != None:
                 yield t
@@ -84,30 +72,13 @@ def get_toots(client, id):
         if i%10 == 0:
             print(i)
 
-def job():
-    logging.info("Background 1 thread running...")
-    api_base_url = "https://botsin.space"
+def job(client):
     scopes = ["read:statuses", "read:accounts", "read:follows", "write:statuses"]
-    spoiler_text = os.environ['cw']
-    visibility = os.environ['visibility']
-    client_id = os.environ['clientid.secret']
-    client_secret = os.environ['clientsecret.secret']
-    access_token = os.environ['accesstoken.secret']
-
-    client = Mastodon(
-            client_id=client_id,
-            client_secret=client_secret,
-            access_token=access_token,
-            api_base_url=api_base_url)
-
-    me = client.account_verify_credentials()
-    following = client.account_following(me.id)
-
-    with open("/data/corpus.txt", "a") as fp:
-        for f in following:
-            for t in get_toots(client, f.id):
-                fp.write(t + "\n")
-
+    if os.stat('/data/corpus.txt').st_size == 0:
+        with open("/data/corpus.txt", "a") as fp:
+            for f in following:
+                for t in get_toots(client, f.id):
+                    fp.write(t + "\n")
     # publishing toot
     with open("/data/corpus.txt") as fp:
         model = markovify.NewlineText(fp.read())
@@ -115,73 +86,45 @@ def job():
     # you will make that damn sentence
     while sentence is None:
         sentence = model.make_sentence(tries=100000)
-    print("tooting:", model)    
+
+    print("Bot shitposting...", sentence)  
     client.status_post(sentence.replace("\0", "\n"),visibility=visibility,spoiler_text=spoiler_text)
 
-def resp(content,st,id):
-    logging.info("Background 1 thread running...")
+def reply(client):
+    replies = [line.strip().replace("\\n", "\n")
+               for line in open("/data/corpus.txt").readlines()]
+    notifications = client.notifications()
+
+    for notification in notifications:    
+        n_id = notification["id"]
+        n_acct = notification.account.acct
+        if notification.type == "mention":
+            random.shuffle(replies)
+            reply = replies[0]
+            time.sleep(15)
+            status = client.status_reply(notification.status,reply, in_reply_to_id = n_id, visibility = visibility)
+        print("Bot responds to mention...", status)
+        client.notifications_clear()
+        client.notifications()
+        
+if __name__ == "__main__":
     api_base_url = "https://botsin.space"
-    scopes = ["read:statuses", "read:accounts", "read:follows", "write:statuses"]
+    scopes = ["read:statuses", "read:accounts", "read:follows", "write:statuses", "read:notifications", "write:notifications"]
     spoiler_text = os.environ['cw']
     visibility = os.environ['visibility']
     client_id = os.environ['clientid.secret']
     client_secret = os.environ['clientsecret.secret']
     access_token = os.environ['accesstoken.secret']
-
+    sleep_duration = os.environ['sleep_duration']
+    sleep_duration=float(sleep_duration)
     client = Mastodon(
             client_id=client_id,
             client_secret=client_secret,
             access_token=access_token,
             api_base_url=api_base_url)
-
     me = client.account_verify_credentials()
-    following = client.account_following(me.id)    
-    req = content.rsplit(">")[-2].split("<")[0].strip() #Supprimer les informations supplémentaires du corps de la réponse
-    r_date = requests.get(url + "?title=" + req, headers="") #Hit api
-    print(req)
-    with open("/data/corpus.txt") as fp:
-        model = markovify.NewlineText(fp.read())
-    sentence = None
-    # you will make that damn sentence
-    while sentence is None:
-        sentence = model.make_sentence(tries=100000)
-    print("tooting:", model)    
-    client.status_reply(st, sentence.replace("\0", "\n"),id, visibility=visibility,spoiler_text=spoiler_text)
-    notif = client.notifications() #Avoir des notifications
-    count = 0
+    following = client.account_following(me.id)
     while True:
-        if notif[count]['type'] == 'mention':
-            if notif[count]['status']['replies_count'] == 0: #Vérifiez si la réponse a déjà été faite
-                content = notif[count]['status']['content']
-                id = notif[count]['status']['account']['username']
-                st = notif[count]['status']
-                main(content, st, id)
-                count += 1
-            else:
-                break
-        else:
-            count += 1
-        count += 1
-    client.stream_user(Stream())
-
-def run_continuously():
-    cease_continuous_run = threading.Event()
-    class ScheduleThread(threading.Thread):
-        @classmethod
-        def run(cls):
-            while not cease_continuous_run.is_set():
-                schedule.run_pending()
-    continuous_thread = ScheduleThread()
-    continuous_thread.start()
-    return cease_continuous_run
-
-if __name__ == "__main__":
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
-    logging.info("Main - starting thread")
-    schedule.every(3).hours.until(timedelta(hours=12)).do(job)
-    schedule.every(1).hours.do(resp)           
-    stop_run_continuously = run_continuously()
-    logging.info("Main: completed !!")
-    time.sleep(5)
-    stop_run_continuously.set()
+        reply(client)    
+        job(client)
+        time.sleep(sleep_duration)
