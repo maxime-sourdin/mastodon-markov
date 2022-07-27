16df5807930da9c002126a7335f4c89d0d01d3e3
Maxime SOURDIN <101416403+maxime-sourdin@users.noreply.github.com>
2022-07-27 21:22:00 +0200

    syntax

--- main.py
+++ main.py
@@ -7,33 +7,26 @@ def parse_toot(toot):
     if toot.reblog is not None: return
     if toot.visibility not in ["public", "unlisted"]: return
     soup = BeautifulSoup(toot.content, "html.parser")
-
     # we will destroy the mentions until we're ready to use them
     # someday turbocat, you will talk to your sibilings
     for mention in soup.select("span.h-card"):
         mention.decompose()
-
     # make all linebreaks actual linebreaks
     for lb in soup.select("br"):
         lb.insert_after("\n")
         lb.decompose()
-
     # make each p element its own line because sometimes they decide not to be
     for p in soup.select("p"):
         p.insert_after("\n")
         p.unwrap()
-
     # keep hashtags in the toots
     for ht in soup.select("a.hashtag"):
         ht.unwrap()
-
     # unwrap all links (i like the bots posting links)
     for link in soup.select("a"):
         link.insert_after(link["href"])
         link.decompose()
-
     text = map(lambda a: a.strip(), soup.get_text().strip().split("\n"))
-
     # next up: store this and patch markovify to take it
     # return {"text": text, "mentions": mentions, "links": links}
     return "\0".join(list(text))
@@ -53,7 +46,6 @@ def get_toots(client, id):
 
 def job(client):
     while True: 
-        scopes = ["read:statuses", "read:accounts", "read:follows", "write:statuses"]
         with open("/data/corpus.txt", "a") as fp:
             for f in following:
                 for t in get_toots(client, f.id):
@@ -74,7 +66,6 @@ def reply(client):
         replies = [line.strip().replace("\\n", "\n")
                 for line in open("/data/corpus.txt").readlines()]
         notifications = client.notifications()
-
         for notification in notifications:    
             n_id = notification["id"]
             n_acct = notification.account.acct
@@ -88,7 +79,6 @@ def reply(client):
         
 if __name__ == "__main__":
     api_base_url = "https://botsin.space"
-    scopes = ["read:statuses", "read:accounts", "read:follows", "write:statuses", "read:notifications", "write:notifications"]
     spoiler_text = os.environ['cw']
     visibility = os.environ['visibility']
     client_id = os.environ['clientid.secret']
@@ -96,14 +86,9 @@ if __name__ == "__main__":
     access_token = os.environ['accesstoken.secret']
     sleep_duration = os.environ['sleep_duration']
     sleep_duration=float(sleep_duration)
-    client = Mastodon(
-            client_id=client_id,
-            client_secret=client_secret,
-            access_token=access_token,
-            api_base_url=api_base_url)
+    client = Mastodon(client_id=client_id,client_secret=client_secret,access_token=access_token,api_base_url=api_base_url)
     me = client.account_verify_credentials()
     following = client.account_following(me.id)
-
     # threading
     answer = threading.Thread(target=reply, args=(client,))
     generate = threading.Thread(target=job, args=(client,))
