178659ec09817b332546b81ee27b27f4123cefec
maxime@sourdin.ovh <maxime@sourdin.ovh>
2022-08-07 15:54:22 +0200

    fix memory/cpu usage

--- main.py
+++ main.py
@@ -8,52 +8,67 @@ class MastodonConfigurationError(Exception):
 
 def parse_toot(toot):
     try: 
-        if toot.spoiler_text != "": return
-        if toot.reblog is not None: return
-        if toot.visibility not in ["public", "unlisted"]: return
-        soup = BeautifulSoup(toot.content, "html.parser")
-        # we will destroy the mentions until we're ready to use them
-        # someday turbocat, you will talk to your sibilings
-        for mention in soup.select("span.h-card"):
-            mention.decompose()
-        # make all linebreaks actual linebreaks
-        for lb in soup.select("br"):
-            lb.insert_after("\n")
-            lb.decompose()
-        # make each p element its own line because sometimes they decide not to be
-        for p in soup.select("p"):
-            p.insert_after("\n")
-            p.unwrap()
-        # keep hashtags in the toots
-        for ht in soup.select("a.hashtag"):
-            ht.unwrap()
-        # unwrap all links (i like the bots posting links)
-        for link in soup.select("a"):
-            link.insert_after(link["href"])
-            link.decompose()
-        text = map(lambda a: a.strip(), soup.get_text().strip().split("\n"))
-        # next up: store this and patch markovify to take it
-        # return {"text": text, "mentions": mentions, "links": links}
-        return "\0".join(list(text))
-        print(time.strftime("%H:%M:%S"), " - Parsing toots...") 
+        if client.ratelimit_remaining > 10:
+            if toot.spoiler_text != "": return
+            if toot.reblog is not None: return
+            if toot.visibility not in ["public", "unlisted"]: return
+            soup = BeautifulSoup(toot.content, "html.parser")
+            # we will destroy the mentions until we're ready to use them
+            # someday turbocat, you will talk to your sibilings
+            for mention in soup.select("span.h-card"):
+                mention.decompose()
+            # make all linebreaks actual linebreaks
+            for lb in soup.select("br"):
+                lb.insert_after("\n")
+                lb.decompose()
+            # make each p element its own line because sometimes they decide not to be
+            for p in soup.select("p"):
+                p.insert_after("\n")
+                p.unwrap()
+            # keep hashtags in the toots
+            for ht in soup.select("a.hashtag"):
+                ht.unwrap()
+            # unwrap all links (i like the bots posting links)
+            for link in soup.select("a"):
+                link.insert_after(link["href"])
+                link.decompose()
+            text = map(lambda a: a.strip(), soup.get_text().strip().split("\n"))
+            # next up: store this and patch markovify to take it
+            # return {"text": text, "mentions": mentions, "links": links}
+            return "\0".join(list(text))
+            print(time.strftime("%H:%M:%S"), " - Parsing toots...")
+            print(time.strftime("%H:%M:%S"), " - Remaining request:", client.ratelimit_remaining)        
+            return
+        else:
+            print(time.strftime("%H:%M:%S"), " - Low remaining request:", client.ratelimit_remaining)          
+            pid = os.getpid()
+            os.kill(pid, signal.SIGTERM)                
     except:
-        print(time.strftime("%H:%M:%S"), " - Failed to parse toots!") 
+        print(time.strftime("%H:%M:%S"), " - Failed to parse toots!")
+        print(time.strftime("%H:%M:%S"), " - Remaining request:", client.ratelimit_remaining)          
         pid = os.getpid()
         os.kill(pid, signal.SIGTERM)
 
 def get_toots(client, id):
-    i = 0
-    toots = client.account_statuses(id,only_media=False,pinned=False,exclude_replies=False, limit="1")
-    while toots is not None and len(toots) > 1:
-        for toot in toots:
-            t = parse_toot(toot)
-            if t != None:
-                yield t
-        toots = client.fetch_next(toots)
-        i += 1
-        print(time.strftime("%H:%M:%S"), " - Getting toots (",i,")")
+    if client.ratelimit_remaining > 10:
+        i = 0
+        toots = client.account_statuses(id,only_media=False,pinned=False,exclude_replies=False, limit="1")
+        while toots is not None and len(toots) > 1:
+            for toot in toots:
+                t = parse_toot(toot)
+                if t != None:
+                    yield t
+            toots = client.fetch_next(toots)
+            i += 1
+            print(time.strftime("%H:%M:%S"), " - Getting toots (",i,")")
+            print(time.strftime("%H:%M:%S"), " - Remaining request:", client.ratelimit_remaining)          
+            return
+    else:
+        print(time.strftime("%H:%M:%S"), " - Low remaining request:", client.ratelimit_remaining)          
+        pid = os.getpid()
+        os.kill(pid, signal.SIGTERM)
 
-def write_toot(client):
+def write_toot(client):    
     i = 0       
     # Get toots and store it on file
     with open(corpus_location, "a") as fp:
@@ -62,22 +77,32 @@ def write_toot(client):
                 fp.write(t + "\n")
                 i += 1                        
                 print(time.strftime("%H:%M:%S"), "- Saving toots (",i,")")
+                print(time.strftime("%H:%M:%S"), " - Remaining request:", client.ratelimit_remaining)                  
+                return
 
 def job(client):
     while True:
-        with open(corpus_location) as fp:
-            model = markovify.NewlineText(fp.read())
-            sentence = None               
-            while sentence is None:
-                sentence = model.make_short_sentence(tries=tries, max_chars=max_chars, min_chars=min_chars)
-                sentence = sentence.replace("\0", "\n")          
-        status = client.status_post(sentence, visibility = visibility, spoiler_text=spoiler_text)
-        print("Next line you're going to say:" "", sentence, "")                                
-        print(time.strftime("%H:%M:%S"), "- Sleeping", sleep_duration, " seconds...")
-        time.sleep(sleep_duration)                              
+        if client.ratelimit_remaining > 10:        
+            with open(corpus_location) as fp:
+                model = markovify.NewlineText(fp.read())
+                sentence = None               
+                while sentence is None:
+                    sentence = model.make_short_sentence(tries=tries, max_chars=max_chars, min_chars=min_chars)
+                    sentence = sentence.replace("\0", "\n")          
+            status = client.status_post(sentence, visibility = visibility, spoiler_text=spoiler_text)
+            print("Next line you're going to say:" "", sentence, "")                                
+            print(time.strftime("%H:%M:%S"), "- Sleeping", sleep_duration, " seconds...")
+            print(time.strftime("%H:%M:%S"), " - Remaining request:", client.ratelimit_remaining)      
+            time.sleep(sleep_duration)
+            return                          
+        else:
+            print(time.strftime("%H:%M:%S"), " - Low remaining request:", client.ratelimit_remaining)          
+            pid = os.getpid()
+            os.kill(pid, signal.SIGTERM)
 
 def answer(client):
     while True:
+        if client.ratelimit_remaining > 10:            
             notifications = client.notifications()
             for notification in notifications:            
                 n_id = notification["id"]
@@ -94,8 +119,14 @@ def answer(client):
                             print("Notification", n_id, "from", n_acct, "treated")
                             print("Next line you're going to say:" "", reply, "")
                             print("Sleeping 60 seconds...")
+                            print(time.strftime("%H:%M:%S"), " - Remaining request:", client.ratelimit_remaining)  
                             time.sleep(60)
-  
+                            return
+        else:
+            print(time.strftime("%H:%M:%S"), " - Low remaining request:", client.ratelimit_remaining)          
+            pid = os.getpid()
+            os.kill(pid, signal.SIGTERM)
+                        
 if __name__ == "__main__":
     print(time.strftime("%H:%M:%S"), "- Bot started !")
     client_secret: Union[str, Any] = os.environ.get("client_secret", default=None)
